339f24a512a53d21164708ebdd48b9759be59da4
gokulhansv <gokulhansv@gmail.com>
2024-01-15 09:37:13 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,7 @@ import Home from "./pages/Home/Home";
 const queryClient = new QueryClient();
 
 function App() {
-  const fetchDrop = async () => {
-    const response = await axiosClient.get(`/`);
-    console.log(response.data.data);
-  };
-
+  
   return (
     <>
       <QueryClientProvider client={queryClient}>
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -13,6 +13,25 @@ const Home = () => {
     return response.data;
   };
 
+  // const mutation = useMutation({
+  //   mutationFn: postTodo,
+  //   onSuccess: () => {
+  //     // Invalidate and refetch
+  //     queryClient.invalidateQueries({ queryKey: ["todos"] });
+  //   },
+  // });
+
+  // <button
+  //   onClick={() => {
+  //     mutation.mutate({
+  //       id: Date.now(),
+  //       title: "Do Laundry",
+  //     });
+  //   }}
+  // >
+  //   Add Todo
+  // </button>;
+
   const { isLoading, isError, error, data } = useQuery({
     queryKey: ["test"],
     queryFn: fetchDrops,
